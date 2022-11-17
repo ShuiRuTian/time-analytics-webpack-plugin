24f0f27dcf2c2c745849711695bed970e694f844
ShuiRuTian <158983297@qq.com>
2022-11-17 14:57:11 +0800

    prepare for test

--- test/entry.test.ts
+++ test/entry.test.ts
@@ -1,7 +1,24 @@
 import { PACKAGE_NAME } from '@src/const';
+import { readdirSync, statSync } from 'fs-extra';
+import { MONOREPO_FOLDER_PATH, repoInit } from './util';
 
 describe(PACKAGE_NAME, () => {
-  it('should say hello', () => {
-    console.log('hello from jest!');
+  const allTestRepoPaths: string[] = [];
+  readdirSync(MONOREPO_FOLDER_PATH).forEach(filePath => {
+    const stat = statSync(filePath);
+    if (stat.isDirectory() && !filePath.includes('node_modules')) {
+      allTestRepoPaths.push(filePath);
+    }
+  });
+
+  allTestRepoPaths.forEach(repoPath => {
+    // 
+  });
+
+  describe('repo1', () => {
+    repoInit(`${MONOREPO_FOLDER_PATH}/repo1`);
+    test('hello', () => {
+      console.log('hello');
+    })
   });
 });
--- test/util.ts
+++ test/util.ts
@@ -1,9 +1,18 @@
-import { assert } from "@src/utils";
-import { existsSync } from "fs";
-import path from "path";
+import { assert } from '../src/utils';
+import { existsSync } from 'fs';
+import { copySync } from 'fs-extra';
+import path from 'path';
 
 const MONOREPO_FOLDER_PATH = path.join(__dirname, 'monorepos');
+const COMMON_FOLDER_PATH = path.join(__dirname, 'common');
 
-assert(existsSync(MONOREPO_FOLDER_PATH), 'could not found monorepo folder, which is the root of each real test cases');
+
+assert(existsSync(COMMON_FOLDER_PATH), 'could not found "common" folder, which contains common files');
+assert(existsSync(MONOREPO_FOLDER_PATH), 'could not found "monorepo" folder, which is the root of each real test cases');
+
+export function repoInit(repoFolder: string) {
+    // move all files from 'common' to `repoFolder`
+    copySync(COMMON_FOLDER_PATH, repoFolder);
+}
 
 export { MONOREPO_FOLDER_PATH };
