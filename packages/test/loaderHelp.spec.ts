60b2572ed1eb64b32e324039e1d86205c85b49db
ShuiRuTian <158983297@qq.com>
2022-12-07 16:54:33 +0800

    Fix test in windows

--- packages/test/loaderHelp.spec.ts
+++ packages/test/loaderHelp.spec.ts
@@ -4,10 +4,12 @@ import { getLoaderName } from 'time-analytics-webpack-plugin/loaderHelper';
 
 const NODE_MODULES_PATH = 'node_modules';
 
+const canonicalPath = (pathS: string) => pathS.replace(/\\/g, '/');
+
 describe('getLoaderName function', () => {
     describe('when the path does not include "node_modules"', () => {
         it('should return the whole path', () => {
-            const testPath = path.join('a', 'b', 'c');
+            const testPath = canonicalPath(path.join('a', 'b', 'c'));
             const loaderName = getLoaderName(testPath);
             expect(loaderName).to.be.string(testPath);
         });
@@ -16,14 +18,14 @@ describe('getLoaderName function', () => {
     describe('when the path includes "node_modules"', () => {
         it('should return the next path afther the last "node_modules"', () => {
             const nextPathAfterLastNodeModules = 'loaderPackageName';
-            const testPath = path.join('root', NODE_MODULES_PATH, 'c', 'd', NODE_MODULES_PATH, nextPathAfterLastNodeModules, 'f');
+            const testPath = canonicalPath(path.join('root', NODE_MODULES_PATH, 'c', 'd', NODE_MODULES_PATH, nextPathAfterLastNodeModules, 'f'));
             const loaderName = getLoaderName(testPath);
             expect(loaderName).to.be.string(nextPathAfterLastNodeModules);
         });
 
         it('should return one more path if the name includes "@"', () => {
-            const nextPathAfterLastNodeModules = path.join('@foo', 'loaderPackageName');
-            const testPath = path.join('root', NODE_MODULES_PATH, 'c', 'd', NODE_MODULES_PATH, nextPathAfterLastNodeModules, 'f');
+            const nextPathAfterLastNodeModules = canonicalPath(path.join('@foo', 'loaderPackageName'));
+            const testPath = canonicalPath(path.join('root', NODE_MODULES_PATH, 'c', 'd', NODE_MODULES_PATH, nextPathAfterLastNodeModules, 'f'));
             const loaderName = getLoaderName(testPath);
             expect(loaderName).to.be.string(nextPathAfterLastNodeModules);
         });
